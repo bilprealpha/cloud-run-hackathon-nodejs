c607c7205de73dc35e1cfa588ebef77c3760db38
Bilal Asadi <bilal.asadi@niveussolutions.com>
2022-11-18 13:43:32 +0530

    more consoles

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+let _isMoved = true
+let _lastMove = ''
+let _lastDim = []
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
@@ -9,13 +11,19 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
+
   console.log(JSON.stringify(req.body));
-  let dims = req.body.jsonPayload.arena.dims;
-  let state = req.body.jsonPayload.arena.state;
+  let dims = req.body.arena.dims;
+  let state = req.body.arena.state;
   console.log('dims ',JSON.stringify(dims));
   console.log('state ',JSON.stringify(state));
   const moves = ['F', 'T', 'L', 'R'];
-  
+  _lastMove = moves
+  _lastDim = dims
+  _isMoved = !_isMoved
+  console.log('last move ',_lastMove);
+  console.log('last dim ',_lastDim);
+  console.log('is moved ',_isMoved);
   // TODO add your implementation here to replace the random response
   
   res.send(moves[Math.floor(Math.random() * moves.length)]);
