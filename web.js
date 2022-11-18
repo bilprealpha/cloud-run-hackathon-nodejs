4bb49acf33b9a0e0520ac24a320ae0eb3c3171ef
Bilal Asadi <bilal.asadi@niveussolutions.com>
2022-11-18 12:59:29 +0530

    more consoles

--- web.js
+++ web.js
@@ -10,6 +10,10 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   console.log(JSON.stringify(req.body));
+  let dims = req.body.jsonPayload.arena.dims;
+  let state = req.body.jsonPayload.arena.state;
+  console.log('dims ',JSON.stringify(dims));
+  console.log('state ',JSON.stringify(state));
   const moves = ['F', 'T', 'L', 'R'];
   
   // TODO add your implementation here to replace the random response
