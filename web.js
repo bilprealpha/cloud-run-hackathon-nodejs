b03a1462367d28be80085a6d9f92543702d437ac
Bilal Asadi <bilal.asadi@niveussolutions.com>
2022-11-18 15:27:08 +0530

    changes

--- web.js
+++ web.js
@@ -1,17 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const { MIN, MAX, RIGHTMAX, BOTTOMAX, DIRECTION, MOVES, END } = require('./constants');
-const e = require('express');
-let _isMoved = false
-let _lastMove = ''
-let _lastDim = []
-let _isTurned = false
-let lastMovedCount = 0
-let totalAttacks = 0
-let totalMoves = 0
-let lastscore = 0
-const myUrl = 'https://cloud-run-hackathon-nodejs-wg35wopo7q-uc.a.run.app'
+
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
@@ -19,156 +9,12 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  console.log('body',JSON.stringify(req.body));
-  console.log('last move ',_lastMove);
-  console.log('last dim ',_lastDim);
-  console.log('is moved ',_isMoved);
-  let state = req.body.arena.state;
-  let newMove = null
-
- let myDirection = state[myUrl].direction;
- let newScore = state[myUrl].score;
- let dims = [state[myUrl].x, state[myUrl].y];
-
- console.log('new score ',newScore, 'last score ',lastscore);
- if(newScore >= lastscore){
-  lastscore = newScore
-  if(_isMoved){
-    console.log('nomove')
-    newMove = MOVES.T
-   _isMoved = false
-  }
-  else{    
-
-    console.log('nomove Attack')
-    newMove = MOVES.R
-    _isMoved = true
-  }
-
-  res.send(newMove);
-  _lastMove = newMove
-  _lastDim = dims
-
-  return
- }
- lastscore = newScore
-  console.log('dims ',JSON.stringify(dims));
-  console.log('myDirection ',JSON.stringify(myDirection));
-  const moves = ['F', 'L', 'R'];
-  const attack  = 'T'
-  if(_lastMove === 'R' || _lastMove === 'L'){
-    _isTurned = false
-    newMove = 'F'
-    lastMovedCount++
-    _isMoved = true
-    totalMoves++
-  _lastMove = newMove
-  _lastDim = dims
-    res.send(newMove);
-    return
-  }
-  if(dims === MIN){
-    if(myDirection === 'S' || myDirection === 'E'){
-      newMove = MOVES.F
-    }else{      
-      newMove = MOVES.R
-      _isTurned = true
-    }
-  }
-  else if(dims === RIGHTMAX){
-    if(myDirection === 'S' || myDirection === 'E'){
-      newMove = MOVES.F
-      }else{
-        newMove = MOVES.R
-        _isTurned = true
-      }
-  }
-  else if(dims === MAX){
-    if(myDirection === 'N' || myDirection === 'W'){
-      newMove = MOVES.F
-      }else{
-        newMove = MOVES.R
-        _isTurned = true
-      }
-
-  }
-  else if(dims === BOTTOMAX){
-    if(myDirection === 'N' || myDirection === 'E'){
-      newMove = MOVES.F
-      }else{
-        newMove = MOVES.R
-        _isTurned = true
-      }
-  }
-  if(newMove && lastMovedCount<3){
-    console.log('calculated')
-    res.send(newMove);
-    lastMovedCount++
-    totalMoves++
-    _isMoved = true
-  }
-  else if(lastMovedCount==0){
-    if(dims[0]>=12 && dims[1]<=3){
-      newMove = MOVES.R
-      lastMovedCount++
-      totalMoves++
-      _isMoved = true
-      _isTurned = true
-    }
-    else if(dims[0]<=3 && dims[1]<=3){
-      newMove = MOVES.R
-      lastMovedCount++
-      totalMoves++
-      _isMoved = true
-      _isTurned = true
-    }
-
-    else if(dims[0]<=3 && dims[1]<=12){
-      newMove = MOVES.L
-      lastMovedCount++
-      totalMoves++
-      _isMoved = true
-      _isTurned = true
-    }
-
-    else if(dims[0]<=12 && dims[1]<=12){
-      newMove = MOVES.R
-      lastMovedCount++
-      totalMoves++
-      _isMoved = true
-      _isTurned = true
-    }}
-   
-  if(newMove){
-
-    // moves[Math.floor(Math.random() * moves.length)]
-    lastMovedCount++
-    _isMoved = true
-    totalMoves++
-    res.send(newMove);
-    }
-
-  else{
-    if(lastscore>newScore){
-      newMove = moves[Math.floor(Math.random() * moves.length)]
-      lastMovedCount++
-      _isMoved = true
-      totalMoves++
-    res.send(newMove);
-    }
-    else{
-      console.log('attack')
-      lastMovedCount = 0
-      _isMoved = false
-      totalAttacks++
-      res.send(attack);
-    }
-  }
-  _lastMove = newMove
-  _lastDim = dims
-  }
+  console.log(req.body);
+  const moves = ['F', 'T', 'L', 'R'];
   
   // TODO add your implementation here to replace the random response
-);
+  
+  res.send(moves[Math.floor(Math.random() * moves.length)]);
+});
 
 app.listen(process.env.PORT || 8080);
